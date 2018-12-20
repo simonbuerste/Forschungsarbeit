50ce773b4fb8c8576deed4b8d943a30f1c6b4a99
simonbuerste <44294097+simonbuerste@users.noreply.github.com>
2018-12-20 14:00:05 +0100

    implement TFRecords

---
+++ convert_to_records.py
@@ -0,0 +1,60 @@
+""" Convert MNIST data to TFRecords file format with Example protos."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+
+import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.datasets import mnist
+
+data_dir = 'C:/Users/simon/Documents/Uni_Stuttgart/Forschungsarbeit/Code/Data/MNIST'
+#data_dir = os.path.join(os.path.expanduser('~'), 'no_backup', 's1279', 'MNIST_data')
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def convert_to(data_set, name):
+    """Converts a dataset to tfrecords"""
+    images = data_set.images
+    labels = data_set.labels
+    num_samples = data_set.num_examples
+
+    rows = images.shape[1]
+    cols = images.shape[2]
+    depth = images.shape[3]
+
+    filename = os.path.join(data_dir, name + '.tfrecords')
+    with tf.python_io.TFRecordWriter(filename) as writer:
+        for index in range(num_samples):
+            image_raw = images[index].tostring()
+            example = tf.train.Example(
+                features=tf.train.Features(
+                    feature={
+                        'height':   _int64_feature(rows),
+                        'width':    _int64_feature(cols),
+                        'depth':    _int64_feature(depth),
+                        'label':    _int64_feature(int(labels[index])),
+                        'image':    _bytes_feature(image_raw)
+                    }))
+            writer.write(example.SerializeToString())
+
+
+# Get the data
+dataset = mnist.read_data_sets(data_dir,
+                               dtype=tf.uint8,
+                               reshape=False)
+
+# Convert and write result to TFRecords
+convert_to(dataset.train, 'train')
+convert_to(dataset.validation, 'validation')
+convert_to(dataset.test, 'test')
--- input_fn.py
+++ input_fn.py
@@ -4,15 +4,39 @@ Set up Iterators for batching and Shuffling of Data.
 """
 
 import tensorflow as tf
+import os
 
 
-def input_fn(mode, data, params):
+def extract_fn(tfrecord):
+    # Extract features using the keys set during creation
+    features = {
+        'image':       tf.FixedLenFeature([], tf.string),
+        'label':     tf.FixedLenFeature([], tf.int64),
+        'height':    tf.FixedLenFeature([], tf.int64),
+        'width':     tf.FixedLenFeature([], tf.int64),
+        'depth':     tf.FixedLenFeature([], tf.int64)
+    }
+
+    # Extract the data record
+    sample = tf.parse_single_example(tfrecord, features)
+
+    img_shape = tf.stack([sample['height'], sample['width'], sample['depth']])
+    image = tf.decode_raw(sample['image'], tf.uint8)
+    image = tf.cast(image, tf.float32)
+    image = image/255  # ensuring value range between 0 and 1
+    #image = tf.reshape(image, img_shape)
+
+    label = sample['label']
+    return [image, label, img_shape]
+
+
+def input_fn(data_dir, mode, params):
     """ Input Function for the Model
 
     Args:
         mode: (string) 'train' 'eval' or something similar. At Training, Data will be shuffled and we have multiple epochs
-        data: (tf.Dataset) COntains the Data
-        Params: (Parameters) contains Parameters relevant for Data Preparation (params.num_epochs, params.batch_size,..)
+        data_dir: (tf.Dataset) COntains the directory of the Data
+        params: (Parameters) contains Parameters relevant for Data Preparation (params.num_epochs, params.batch_size,..)
 
     """
 
@@ -21,24 +45,29 @@ def input_fn(mode, data, params):
     else:
         buffer_size = 1
 
-    # Create Dataset and Iterator
+    # Create a list of filenames and pass it to a queue
+    filename = os.path.join(data_dir, mode + '.tfrecords')
+
+    # Pipeline of dataset and iterator
+    dataset = tf.data.TFRecordDataset([filename])
+    dataset = dataset.map(extract_fn)
     # Create Training Dataset, shuffle and batch it
-    data = tf.data.Dataset.from_tensor_slices((data.images, data.labels))
-    data = data.shuffle(buffer_size)    # if you want to shuffle the Data
-    data = data.batch(params["batch_size"])
-    data = data.prefetch(1)                     # make sure always one batch is ready to serve
+    dataset = dataset.shuffle(buffer_size)    # if you want to shuffle the Data
+    dataset = dataset.batch(params["batch_size"])
+    dataset = dataset.prefetch(1)                     # make sure always one batch is ready to serve
 
     # Create initializable iterator from Data so that it can be reset at each epoch
-    iterator = data.make_initializable_iterator()
+    iterator = dataset.make_initializable_iterator()
 
     # Query the Output of the Iterator for input to the model
-    img, label = iterator.get_next()
+    img, label, img_shape = iterator.get_next()
     init_op = iterator.initializer
 
-    # Build and return a dictionnary containing the nodes / ops
+    # Build and return a dictionary containing the nodes / ops
     inputs = {
         'img': img,
         'labels': label,
+        'img_shape': img_shape,
         'iterator_init_op': init_op
     }
 
