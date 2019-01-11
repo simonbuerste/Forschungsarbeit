3608c18868a04e63ef16fe7df56d2436b88a0dee
simonbuerste <44294097+simonbuerste@users.noreply.github.com>
2019-01-11 13:49:35 +0100

    Updates for environment without display properties (i.e. GPU-Server)

--- utils.py
+++ utils.py
@@ -8,6 +8,11 @@ import io
 import umap
 import numpy as np
 import tensorflow as tf
+
+import matplotlib as mpl
+if os.environ.get('DISPLAY', '') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
 import matplotlib.pyplot as plt
 from tensorflow.contrib.tensorboard.plugins import projector
 
