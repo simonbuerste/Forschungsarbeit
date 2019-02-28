18dd9d10e4426ada566109c0f527f46d796a7254
simonbuerste <44294097+simonbuerste@users.noreply.github.com>
2019-02-28 09:08:14 +0100

    implement self attention layer

--- D_AE1.py
+++ D_AE1.py
@@ -6,6 +6,44 @@ def lrelu(x, alpha=0.2):
     return tf.maximum(x, tf.multiply(x, alpha))
 
 
+def selfattentionlayer(x, iteration):
+
+    batch_size, h, w, num_channels = x.get_shape().as_list()
+    location_num = h*w
+    downsampled_num = location_num//4
+
+    k_h = 1
+    k_w = 1
+    d_h = 1
+    d_w = 1
+
+    w_f = tf.get_variable(name='w_f_%d' % iteration, initializer=tf.contrib.layers.xavier_initializer(), shape=[k_h, k_w, x.get_shape()[-1], num_channels//8])
+    w_g = tf.get_variable(name='w_g_%d' % iteration, initializer=tf.contrib.layers.xavier_initializer(), shape=[k_h, k_w, x.get_shape()[-1], num_channels//8])
+    w_h = tf.get_variable(name='w_h_%d' % iteration, initializer=tf.contrib.layers.xavier_initializer(), shape=[k_h, k_w, x.get_shape()[-1], num_channels])
+    w_attn = tf.get_variable(name='w_attn_%d' % iteration, initializer=tf.contrib.layers.xavier_initializer(), shape=[k_h, k_w, x.get_shape()[-1], num_channels])
+
+    theta = tf.nn.conv2d(x, w_f, strides=[1, d_h, d_w, 1], padding='SAME')
+    phi = tf.nn.conv2d(x, w_g, strides=[1, d_h, d_w, 1], padding='SAME')
+    g = tf.nn.conv2d(x, w_h, strides=[1, d_h, d_w, 1], padding='SAME')
+
+    phi = tf.layers.max_pooling2d(inputs=phi, pool_size=[2, 2], strides=2)
+    g = tf.layers.max_pooling2d(inputs=g, pool_size=[2, 2], strides=2)
+
+    theta = tf.reshape(theta, [-1, location_num, num_channels//8])
+    phi = tf.reshape(phi, [-1, downsampled_num, num_channels//8])
+    g = tf.reshape(g, [-1, downsampled_num, num_channels])
+
+    attn = tf.matmul(theta, phi, transpose_b=True)
+    attn = tf.nn.softmax(attn)
+
+    attn_g = tf.matmul(attn, g)
+    attn_g = tf.reshape(attn_g, [-1, h, w, num_channels])
+
+    sigma = tf.get_variable('sigma_ratio_%d' % iteration, [], initializer=tf.constant_initializer(0.0))
+    output = tf.nn.conv2d(attn_g, w_attn, strides=[1, d_h, d_w, 1], padding='SAME')
+    return x + sigma*output
+
+
 # Defining the Encoder
 def encoder(encoder_input, is_training, params):
     x = tf.reshape(encoder_input, shape=[-1, params.resize_height, params.resize_width, params.channels])
@@ -14,6 +52,7 @@ def encoder(encoder_input, is_training, params):
         print(x.get_shape())
         x = tf.layers.conv2d(x, filters=16*(2**k), kernel_size=3, strides=1, padding='same',
                              kernel_initializer=tf.contrib.layers.xavier_initializer())
+        x = selfattentionlayer(x, k)
         x = tf.layers.batch_normalization(x, training=is_training)
         x = tf.nn.leaky_relu(x, alpha=0.2)
 
