3aea718c3e97e8d9bc7177bcbd8d5d6581556962
simonbuerste <simonkamm@online.de>
2019-04-09 15:58:37 +0200

    Updated Execution File

--- Execute.py
+++ Execute.py
@@ -5,13 +5,16 @@ from shutil import copyfile
 
 datasets = ["MNIST", "F-MNIST", "CIFAR-10", "CIFAR-100", "IMAGENET-10", "IMAGENET-Dog"] #"MNIST", "F-MNIST", "CIFAR-10", "CIFAR-100", "IMAGENET-10", "IMAGENET-Dog"] #
 n_latent = 10#5, 10, 20, 32, 64, 128, 256
-lambda_r  =[1, 0.1, 0.01, 0.001]
-alpha = [0, 0.5, 1]
+
+train_batch_size = [64, 128, 256, 512, 1024]
+initial_learning_rate = [0.001, 0.01, 0.1]
+#lambda_r  =[1, 0.1, 0.01, 0.001]
+#alpha = [0, 0.5, 1]
 #temperature_gumbel = [10, 5, 2, 1, 0.5]
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
-filename_params = "params_discr_AE"
+filename_params = "params_VAE"
 tmp_filename = "{}.json".format(filename_params)
 i = 0
 
@@ -64,19 +67,20 @@ for dataset in datasets:
     with open(tmp_filename, "w") as paramsFile:
         json.dump(params, paramsFile)
 
-    for n in lambda_r:
-        for k in alpha:
+    for n in train_batch_size:
+        for k in initial_learning_rate:
             # first read the params and overwrite the specific parameter
             with open(tmp_filename, "r") as paramsFile:
                 params = json.load(paramsFile)
 
             # part for changing the parameters as desired
-            params["lambda_d"] = 1.0
-            params["lambda_r"] = n
-            params["alpha"] = k
+            params["train_batch_size"] = n
+            params["initial_training_rate"] = k
             #params["temperature_gumbel"] = n
             params["n_latent"] = n_latent
-            params["train_batch_size"] = 1000 # fix for beta AE
+            
+            params["lambda_d"] = 1.0
+            #params["train_batch_size"] = 1000 # fix for beta AE
 
             # write the new parameters to the parameter file
             with open(tmp_filename, "w") as paramsFile:
@@ -85,7 +89,7 @@ for dataset in datasets:
             #p = subprocess.Popen('python3.6 Train.py --dataset=MNIST --gpu=2 --latent_model=AE')
             #p.wait()
             for i in range(no_repetitions):
-                os.system('python3.6 Train.py --dataset=%s --gpu=2 --latent_model=b_AE --cluster_model=kmeans --Parameters=%s'% (dataset, tmp_filename))
+                os.system('python3.6 Train.py --dataset=%s --gpu=2 --latent_model=VAE --cluster_model=kmeans --Parameters=%s'% (dataset, tmp_filename))
                 time.sleep(70)  # 70 seconds pause to ensure models are not written in same folder
 
 # remove the copied version of params file,
