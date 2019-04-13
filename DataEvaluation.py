1c54aa59b8abef8bf8efaf807c905f1f5276b774
simonbuerste <44294097+simonbuerste@users.noreply.github.com>
2019-04-13 09:03:48 +0200

    Updated DataEvaluation

--- DataEvaluation.py
+++ DataEvaluation.py
@@ -7,15 +7,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib2tikz import save as savetikz
 
-directory = "C:/Users/simon/Documents/Uni_Stuttgart/Forschungsarbeit/Models/Discriminative_AE"
+directory = "C:/Users/simon/Documents/Uni_Stuttgart/Forschungsarbeit/Models/InitLearningRate"
 
 list_dir = next(os.walk(directory))[1]
 summary = []
 visu_data = []
 dataset_visu = ["MNIST"]
-model_visu = ["discriminative_AE"] # "AE", "VAE", "discriminative_AE", "featureselective_AE", "Gumbel_VAE"
+model_visu = ["AE", "VAE"] # "AE", "VAE", "discriminative_AE", "featureselective_AE", "Gumbel_VAE"
 
-hyperparameters = ["alpha", "lambda_r"]
+hyperparameters = ["train_batch_size", "initial_training_rate"]
 for x in list_dir:
     # define the filenames
     filename_param = os.path.join(directory, x, "params.json")
@@ -25,78 +25,83 @@ for x in list_dir:
     # open and read the corresponding json files
     with open(filename_param, "r") as paramsFile:
         params = json.load(paramsFile)
-    with open(filename_best_weigths, "r") as eval_best_File:
-        eval_best_weights = json.load(eval_best_File)
-    with open(filename_last_weigths, "r") as eval_last_File:
-        eval_last_weights = json.load(eval_last_File)
 
-    # Check the dataset which was evaluated
-    if "MNIST" in x:
-        if "F-MNIST" in x:
-            dataset = "F-MNIST"
-        else:
-            dataset = "MNIST"
-    elif "CIFAR-10" in x:
-        if "CIFAR-100" in x:
-            dataset = "CIFAR-100"
-        else:
-            dataset = "CIFAR-10"
-    elif "IMAGENET-Dog" in x:
-        dataset = "IMAGENET-Dog"
-    elif "IMAGENET-10" in x:
-        dataset = "IMAGENET-10"
+    # Check if results were saved
+    if os.path.isfile(filename_best_weigths):
+        with open(filename_best_weigths, "r") as eval_best_File:
+            eval_best_weights = json.load(eval_best_File)
 
-    # Check the Latent Model which was used
-    if "AE" in x:
-        if "b_AE" in x:
-            latent_model = "discriminative_AE"
-        elif "fs_AE" in x:
-            latent_model = "featureselective_AE"
-        elif "VAE" in x:
-            if "b_VAE" in x:
-                latent_model = "Beta_VAE"
-            elif "g_VAE" in x:
-                latent_model = "Gumbel_VAE"
+        with open(filename_last_weigths, "r") as eval_last_File:
+            eval_last_weights = json.load(eval_last_File)
+
+        # Check the dataset which was evaluated
+        if "MNIST" in x:
+            if "F-MNIST" in x:
+                dataset = "F-MNIST"
             else:
-                latent_model = "VAE"
-        else:
-            latent_model = "AE"
+                dataset = "MNIST"
+        elif "CIFAR-10" in x:
+            if "CIFAR-100" in x:
+                dataset = "CIFAR-100"
+            else:
+                dataset = "CIFAR-10"
+        elif "IMAGENET-Dog" in x:
+            dataset = "IMAGENET-Dog"
+        elif "IMAGENET-10" in x:
+            dataset = "IMAGENET-10"
 
-    # Check the CLuster model which was used
-    if "kmeans" in x:
-        cluster_model = "kmeans"
-    elif "gmm" in x:
-        cluster_model = "gmm"
-    elif "IDEC" in x:
-        cluster_model = "IDEC"
 
-    # Add general metrics/values/strings to dict
-    tmp = {
-        "Output_Folder":            x,
-        "dataset":                  dataset,
-        "latent_model":             latent_model,
-        "cluster_model":            cluster_model,
-        "test_loss_best":           eval_best_weights["loss"],
-        "test_loss_last":           eval_last_weights["loss"],
-        "training_loss_best":       eval_best_weights["training_loss"],
-        "training_loss_last":       eval_last_weights["training_loss"],
-        "test_log_likelihood_best": eval_best_weights["neg_log_likelihood"],
-        "test_log_likelihood_last": eval_last_weights["neg_log_likelihood"],
-        "Accuracy":                 eval_best_weights["Accuracy"],
-        "Accuracy_last":            eval_last_weights["Accuracy"],
-        "NMI":                      eval_best_weights["Normalized Mutual Information"],
-        "NMI_last":                 eval_last_weights["Normalized Mutual Information"],
-        "ARI":                      eval_best_weights["Adjusted Rand Index"],
-        "ARI_last":                 eval_last_weights["Adjusted Rand Index"],
-    }
-    # add specific hyperparameters to dict (Hyperparams defined at beginning of file)
-    for i in hyperparameters:
-        tmp[i] = params[i]
+        # Check the Latent Model which was used
+        if "AE" in x:
+            if "b_AE" in x:
+                latent_model = "discriminative_AE"
+            elif "fs_AE" in x:
+                latent_model = "featureselective_AE"
+            elif "VAE" in x:
+                if "b_VAE" in x:
+                    latent_model = "Beta_VAE"
+                elif "g_VAE" in x:
+                    latent_model = "Gumbel_VAE"
+                else:
+                    latent_model = "VAE"
+            else:
+                latent_model = "AE"
 
-    summary.append(tmp)
-    # Save desired data for visualization
-    if (tmp["dataset"] in dataset_visu) and (tmp["latent_model"] in model_visu):
-        visu_data.append(tmp)
+        # Check the CLuster model which was used
+        if "kmeans" in x:
+            cluster_model = "kmeans"
+        elif "gmm" in x:
+            cluster_model = "gmm"
+        elif "IDEC" in x:
+            cluster_model = "IDEC"
+
+        # Add general metrics/values/strings to dict
+        tmp = {
+            "Output_Folder":            x,
+            "dataset":                  dataset,
+            "latent_model":             latent_model,
+            "cluster_model":            cluster_model,
+            "test_loss_best":           eval_best_weights["loss"],
+            "test_loss_last":           eval_last_weights["loss"],
+            "training_loss_best":       eval_best_weights["training_loss"],
+            "training_loss_last":       eval_last_weights["training_loss"],
+            "test_log_likelihood_best": eval_best_weights["neg_log_likelihood"],
+            "test_log_likelihood_last": eval_last_weights["neg_log_likelihood"],
+            "Accuracy":                 eval_best_weights["Accuracy"],
+            "Accuracy_last":            eval_last_weights["Accuracy"],
+            "NMI":                      eval_best_weights["Normalized Mutual Information"],
+            "NMI_last":                 eval_last_weights["Normalized Mutual Information"],
+            "ARI":                      eval_best_weights["Adjusted Rand Index"],
+            "ARI_last":                 eval_last_weights["Adjusted Rand Index"],
+        }
+        # add specific hyperparameters to dict (Hyperparams defined at beginning of file)
+        for i in hyperparameters:
+            tmp[i] = params[i]
+
+        summary.append(tmp)
+        # Save desired data for visualization
+        if (tmp["dataset"] in dataset_visu) and (tmp["latent_model"] in model_visu):
+            visu_data.append(tmp)
 
 
 # Write summary dict to csv file
@@ -118,8 +123,8 @@ model_color = {
     "Gumbel_VAE":           'black'
 }
 
-plot_metric = np.zeros((len(visu_data)//len(model_visu), len(model_visu), len(metric_visu)), dtype=np.float32)
-plot_params = np.zeros((len(visu_data)//len(model_visu), len(model_visu), len(hyperparameters)), dtype=np.float32)
+plot_metric = np.zeros((len(visu_data), len(model_visu), len(metric_visu)), dtype=np.float32)
+plot_params = np.zeros((len(visu_data), len(model_visu), len(hyperparameters)), dtype=np.float32)
 
 iterator = np.zeros(len(model_visu), dtype=np.int64)
 for _, dict in enumerate(visu_data):
@@ -149,15 +154,24 @@ if len(hyperparameters) == 1:
         savetikz(savepath)
     plt.show(block=True)
 elif len(hyperparameters) == 2:
-    for i, metric in enumerate(metric_visu):
-        fig = plt.figure(i)
-        ax = fig.add_subplot(111)
-        for l, model in enumerate(model_visu):
-            sc = ax.scatter(plot_params[:, l, 0], np.log(plot_params[:, l, 1]), c=plot_metric[:, l, i])
-        plt.colorbar(sc)#, ticks=[0, 0.2, 0.4, 0.6, 0.8, 1])
-        ax.set_xlabel('%s' % hyperparameters[0])
-        ax.set_ylabel('log of %s' % hyperparameters[1])
-        ax.set_title('%s for different %s and %s' % (metric, hyperparameters[0], hyperparameters[1]))
-    plt.show()
+    for l, model in enumerate(model_visu):
+        for i, metric in enumerate(metric_visu):
+            fig = plt.figure(i)
+            ax = fig.add_subplot(111)
+            sc = ax.scatter(plot_params[:, l, 0], np.log10(plot_params[:, l, 1]), c=plot_metric[:, l, i])
+            plt.colorbar(sc)#, ticks=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+            max_metric = max(plot_metric[:, l, i])
+            idx = np.where(plot_metric[:, l, i] == max_metric)
+            xpos = plot_params[idx, l, 0]
+            ypos = np.log10(plot_params[idx, l, 1])
+            #ax.text(xpos, ypos, 'Maximum: %.3f' % max_metric)
+            #arrowprops = {"facecolor": 'black', "shrink": 0.05}
+            #ax.annotate('maximum Value: %.3f' % max_metric, xy=(xpos, ypos), xytext=(xpos, ypos-0.5))#, arrowprops=arrowprops)
+            ax.set_xlabel('%s' % hyperparameters[0])
+            ax.set_ylabel('log_10 of %s' % hyperparameters[1])
+            ax.set_title('%s for different Hyperparameters for %s' % (metric, model))
+            savepath = os.path.join(directory, '%s.tex' % metric)
+            savetikz(savepath)
+        plt.show()
 else:
     print("Sure you want to visualize this?")
